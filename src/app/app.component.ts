3ae412e8f86bb04017687df35ec2c28d6592062c
Sunny Gupta <sunny.gupta@globallogic.com>
2022-08-17 00:59:31 +0530

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import{ init } from "emailjs-com";
+import emailjs from "emailjs-com"
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'emailjs';
+  checkoutForm: FormGroup;
+  constructor(){
+//https://dashboard.emailjs.com/admin/integration
+// emailjs.init(‘’);
+    init("user_6stpaecdQ6yKRGmkwde2q"); // pass your userid here
+  }
 }
